57267171bca4c11d91bdbdc16da618627c70c219
Momenwael21 <waelmomen42@gmail.com>
2022-12-30 20:54:20 +0200

    update new section complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min";
 import "./App.css";
+
 import { NavBar, TryBar, Companies } from "./components/index";
 import {
   Landing,
--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,7 +1,54 @@
 import React from "react";
+import logo from "../../assets/logo.png";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <nav className="navbar navbar-expand-lg">
+      <div className="container">
+        <a className="navbar-brand" href="/#">
+          <img src={logo} alt="" />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse d-flex"
+          id="navbarSupportedContent"
+        >
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="/#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/#">
+                Features
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/#">
+                Solution
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/#">
+                About Us
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
