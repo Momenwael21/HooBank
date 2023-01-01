dcbadeaabace8672899f1c95fe1ed1699c4b5a18
Momenwael21 <waelmomen42@gmail.com>
2023-01-01 16:26:48 +0200

    update complete new section

--- src/container/solutions/Solutions.jsx
+++ src/container/solutions/Solutions.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { TbScan } from "react-icons/tb";
+import { IoMdArrowDropdown } from "react-icons/io";
+
+import { apple, paypal, shopify, graph, visa } from "./imports";
 
 const Solutions = () => {
   return (
@@ -19,7 +23,84 @@ const Solutions = () => {
               </a>
             </div>
           </div>
-          <div className="col-sm-12 col-lg-6"></div>
+          <div className="col-sm-12 col-lg-6">
+            <div className="cards">
+              <div className="content">
+                <div className="circles-back">
+                  <div className="circle1">
+                    <div className="circle2">
+                      <div className="circle3"></div>
+                    </div>
+                  </div>
+                </div>
+                <div className="scan-card">
+                  <div className="icon">
+                    <TbScan fontSize={25} />
+                  </div>
+                  <h5>Scan Credit Cards</h5>
+                  <p>
+                    Scan yourcredit card in <br /> 4 minutes.
+                  </p>
+                </div>
+                <div className="analysis-card">
+                  <div className="head">
+                    <h5 className="title">Online Analysis</h5>
+                    <div className="date">
+                      <p>
+                        1 Month <IoMdArrowDropdown />
+                      </p>
+                    </div>
+                  </div>
+                  <div className="statistics">
+                    <div className="numbers">
+                      <div className="income">
+                        <h6>$ 2,334,67</h6>
+                        <p>Income</p>
+                      </div>
+                      <div className="expenses">
+                        <h6>$ 5.31M</h6>
+                        <p>Expenses</p>
+                      </div>
+                    </div>
+                    <div className="graph">
+                      <img src={graph} alt="graph" />
+                    </div>
+                    <div className="months">
+                      <p>Jan</p>
+                      <p>Feb</p>
+                      <p className="active">Mar</p>
+                      <p>Apr</p>
+                      <p>May</p>
+                      <p>Jun</p>
+                      <p>Jul</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="method-card">
+                  <div className="head">
+                    <h5 className="title">Pay Method</h5>
+                    <div className="icon">
+                      <IoMdArrowDropdown fontSize={25} />
+                    </div>
+                  </div>
+                  <div className="logos">
+                    <div className="paypal logo">
+                      <img src={paypal} alt="company_logo" />
+                    </div>
+                    <div className="apple logo">
+                      <img src={apple} alt="company_logo" />
+                    </div>
+                    <div className="visa logo">
+                      <img src={visa} alt="company_logo" />
+                    </div>
+                    <div className="shopify logo">
+                      <img src={shopify} alt="company_logo" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
---
+++ src/container/solutions/imports.js
@@ -0,0 +1,7 @@
+import apple from "../../assets/apple.png";
+import paypal from "../../assets/paypal-logo.png";
+import shopify from "../../assets/Shopify.png";
+import visa from "../../assets/visa.png";
+import graph from "../../assets/graph.png";
+
+export { apple, paypal, shopify, visa, graph };
